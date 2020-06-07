2f1c0e3800b30276005dd34477d0c205e718ee28
katsushika <marcoderio850@gmail.com>
2020-06-07 17:27:11 -0500

    CloudCore first init structure (not defined yet)

--- model/CloudCore.java
+++ model/CloudCore.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 import org.json.*;
 import controller.Controller;
 
@@ -14,6 +16,25 @@ import controller.Controller;
 public class CloudCore extends Thread {
     private Controller controller;
     private JSONObject config;
+    
+    // Node information
+    private String name;
+    private String systemDirectory;
+    private JSONArray remoteNodes;
+    private JSONObject backupNode;
+
+    // Threads
+    private ConnectionPoint connectionPointThread;
+    private ArrayList<RemoteSender> remoteSenderThreads;
+    private ArrayList<RemoteReceiver> remoteReceiverThreads;
+    private BackupAdmin backupAdminThread;
+    private BackupSlave backupSlaveThread;
+
+    // Master Queue
+    private Queue masterQueue;
+
+    // File Tables Root
+    private FileTable root;
 
     public CloudCore(Controller controller, JSONObject config) {
         this.controller = controller;
@@ -22,6 +43,42 @@ public class CloudCore extends Thread {
 
     @Override
     public void run()  {
-        System.out.println(config);
+        // Establish all configuration parameters
+        name = config.getString("name");
+        systemDirectory = config.getString("path");
+        remoteNodes = (JSONArray) config.get("remote");
+        backupNode = (JSONObject) config.get("backup");
+
+        // Starts all other system sections
+        initConnectionPoint();
+        initRemoteConnections();
+        initBackup();
+        initBackupPoint();
+    }
+
+    // Initialize ConnectionPoint thread to start server and receive connections from other nodes
+    private void initConnectionPoint() {
+        
+    }
+
+    // Initialize all RemoteSenders threads to connect to remote ConnectionPoints
+    private void initRemoteConnections() {
+
+    }
+
+    // Initalize BackupAdmin thread to manage this node's backup
+    private void initBackup() {
+
+    }
+
+    // Intialize BackupSlave thread to receive files to backup from a remote node
+    private void initBackupPoint() {
+
+    }
+
+    // Add RemoteReceiver thread to the list. This method is called by the ConnectionPoint, wich must be
+    // previoulsy created
+    public void addRemoteReceiver(RemoteReceiver thread) {
+
     }
 }
